09c369b68c32d0b9af62ac765ee00b5d90082f2f
BigMangos <ifenghuangxin@gmail.com>
2024-04-27 22:16:28 +0800

    init

---
+++ do/fetch.go
@@ -0,0 +1,115 @@
+package do
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"github/bigmangos/chrome-offline-installer/internal/model"
+	"github/bigmangos/chrome-offline-installer/internal/util"
+	"log/slog"
+	"strconv"
+	"strings"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type info struct {
+	os  string
+	app string
+}
+
+var versionInfo = map[string]info{
+	"win_stable_x86": {os: `platform="win" version="10.0" sp="" arch="x86"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" version="" nextversion="" lang="en" brand=""  installage="-1" installdate="-1" iid="{11111111-1111-1111-1111-111111111111}"`},
+	"win_stable_x64": {os: `platform="win" version="10.0" sp="" arch="x64"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" version="" nextversion="" lang="en" brand=""  installage="-1" installdate="-1" iid="{11111111-1111-1111-1111-111111111111}"`},
+	"win_beta_x86":   {os: `platform="win" version="10.0" arch="x86"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="1.1-beta"`},
+	"win_beta_x64":   {os: `platform="win" version="10.0" arch="x64"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-beta-multi-chrome"`},
+	"win_dev_x86":    {os: `platform="win" version="10.0" arch="x86"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="2.0-dev"`},
+	"win_dev_x64":    {os: `platform="win" version="10.0" arch="x64"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-dev-multi-chrome"`},
+	"win_canary_x86": {os: `platform="win" version="10.0" arch="x86"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x86-canary"`},
+	"win_canary_x64": {os: `platform="win" version="10.0" arch="x64"`, app: `appid="{8A69D345-D564-463C-AFF1-A69D9E530F96}" ap="x64-canary"`},
+}
+
+const (
+	updateUrl = "https://tools.google.com/service/update2"
+)
+
+func post(os, app string) (string, error) {
+	xml := fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
+    <request protocol="3.0" updater="Omaha" updaterversion="1.3.36.372" shell_version="1.3.36.352" ismachine="0" sessionid="{11111111-1111-1111-1111-111111111111}" installsource="taggedmi" requestid="{11111111-1111-1111-1111-111111111111}" dedup="cr" domainjoined="0">
+    <hw physmemory="16" sse="1" sse2="1" sse3="1" ssse3="1" sse41="1" sse42="1" avx="1"/>
+    <os %s/>
+    <app %s>
+    <updatecheck/>
+    <data name="install" index="empty"/>
+    </app>
+    </request>`, os, app)
+
+	client := resty.New()
+	resp, err := client.R().
+		SetBody(xml).
+		Post(updateUrl)
+	if err != nil {
+		return "", errors.New("post error: " + err.Error())
+	}
+
+	return resp.String(), nil
+}
+
+func decode(input string) (*model.ChromeInstallerInfo, error) {
+	r := &model.Response{}
+	if err := xml.NewDecoder(strings.NewReader(input)).Decode(r); err != nil {
+		return nil, errors.New("decode error: " + err.Error())
+	}
+
+	//fmt.Printf("xml decode: %#v\n", r)
+
+	if len(r.App.Updatecheck.Manifest.Packages.Package) == 0 || len(r.App.Updatecheck.URLs.URL) == 0 {
+		return nil, errors.New(fmt.Sprintf("no package, response: %+v", r))
+	}
+
+	packageFirst := r.App.Updatecheck.Manifest.Packages.Package[0]
+
+	hash := packageFirst.Hash
+	decodedHash, err := base64.StdEncoding.DecodeString(hash)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("decode hash error: %+v", err))
+	}
+
+	size, err := strconv.Atoi(packageFirst.Size)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("parse size error: %+v", err))
+	}
+
+	urls := make([]string, 0, len(r.App.Updatecheck.URLs.URL))
+	for _, url := range r.App.Updatecheck.URLs.URL {
+		urls = append(urls, url.Codebase+packageFirst.Name)
+	}
+
+	return &model.ChromeInstallerInfo{
+		Version: r.App.Updatecheck.Manifest.Version,
+		Size:    size,
+		Sha1:    hex.EncodeToString(decodedHash),
+		Sha256:  packageFirst.HashSha256,
+		Urls:    urls,
+	}, nil
+}
+
+func FetchAndUpdateData(data map[string]*model.ChromeInstallerInfo) {
+	for k, v := range versionInfo {
+		res, err := post(v.os, v.app)
+		if err != nil {
+			slog.Error(fmt.Sprintf("post error: %v", err))
+			continue
+		}
+		decodedRes, err := decode(res)
+		if err != nil {
+			slog.Error(fmt.Sprintf("decode error: %v", err))
+			continue
+		}
+		if data[k] == nil || !util.IsNewVersion(decodedRes.Version, data[k].Version) {
+			data[k] = decodedRes
+		}
+	}
+}
---
+++ do/save.go
@@ -0,0 +1,117 @@
+package do
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github/bigmangos/chrome-offline-installer/internal/model"
+	"log/slog"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	dataPath   = "data.json"
+	readmePath = "README.md"
+)
+
+var verList = []string{
+	"win_stable_x64",
+	"win_stable_x86",
+	"win_beta_x64",
+	"win_beta_x86",
+	"win_dev_x64",
+	"win_dev_x86",
+	"win_canary_x64",
+	"win_canary_x86",
+}
+
+func LoadSavedData() map[string]*model.ChromeInstallerInfo {
+	data := make(map[string]*model.ChromeInstallerInfo)
+	f, err := os.ReadFile(dataPath)
+	if err != nil {
+		slog.Error(fmt.Sprintf("read file error: %v", err))
+		return data
+	}
+
+	if err = json.Unmarshal(f, &data); err != nil {
+		slog.Error(fmt.Sprintf("unmarshal error: %v", err))
+		return data
+	}
+
+	//fmt.Printf("json load: %#v\n", data)
+	return data
+}
+
+func SaveData(newData map[string]*model.ChromeInstallerInfo) error {
+	f, err := os.Create(dataPath)
+	defer f.Close()
+	if err != nil {
+		return errors.New(fmt.Sprintf("create file error: %v", err))
+	}
+
+	r, err := json.Marshal(&newData)
+	if err != nil {
+		return errors.New(fmt.Sprintf("unmarshal error: %v", err))
+	}
+
+	if _, err = f.Write(r); err != nil {
+		return errors.New(fmt.Sprintf("write file error: %v", err))
+	}
+
+	return nil
+}
+
+func SaveMarkdown(data map[string]*model.ChromeInstallerInfo) error {
+	indexUrl := "https://github.com/bigmangos/chrome-offline-installer?tab=readme-ov-file#"
+	f, err := os.Create(readmePath)
+	defer f.Close()
+	if err != nil {
+		return errors.New(fmt.Sprintf("create file error: %v", err))
+	}
+
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return errors.New(fmt.Sprintf("load location error: %v", err))
+	}
+
+	var buf strings.Builder
+	buf.WriteString("# Google Chrome 离线安装包\n")
+	buf.WriteString("本工程是 [Bush2021/chrome_installer](https://github.com/Bush2021/chrome_installer) Go实现，感谢原作者\n\n")
+	buf.WriteString("稳定版存档：<https://github.com/bigmangos/chrome-offline-installer/releases>\n\n")
+	buf.WriteString("最近一次检测更新时间（UTC+8）：\n")
+	buf.WriteString(time.Now().In(loc).Format("2006-01-02 15:04:05"))
+
+	buf.WriteString("\n\n## 目录\n")
+	for _, v := range verList {
+		link := indexUrl + v
+		buf.WriteString(fmt.Sprintf("* [%s](%s)\n", v, link))
+	}
+	buf.WriteString("\n")
+	for _, v := range verList {
+		d, ok := data[v]
+		if !ok {
+			slog.Error(fmt.Sprintf("not found: %s", v))
+			continue
+		}
+		buf.WriteString(fmt.Sprintf("## %s\n", v))
+		buf.WriteString(fmt.Sprintf("**最新版本**： %s  \n", d.Version))
+		buf.WriteString(fmt.Sprintf("**文件大小**： %0.2f MB  \n", formatSize(d.Size)))
+		buf.WriteString(fmt.Sprintf("**校验值（Sha256）**： %s  \n", d.Sha256))
+		buf.WriteString("**下载链接**：\n")
+		for _, url := range d.Urls {
+			buf.WriteString(fmt.Sprintf("%s\n", url))
+		}
+	}
+
+	if _, err := f.WriteString(buf.String()); err != nil {
+		return errors.New(fmt.Sprintf("write file error: %v", err))
+	}
+
+	return nil
+}
+
+func formatSize(in int) float64 {
+	return float64(in) / 1024 / 1024
+}
